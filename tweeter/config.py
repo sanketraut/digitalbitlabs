a8a95af2b79803127a0930bd8950785dc6ec3fbf
Sanket Raut <sanket@digitalbit.in>
2019-08-03 13:35:47 +0530

    Create config.py

---
+++ tweeter/config.py
@@ -0,0 +1,7 @@
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+terms = ['Technology','Artificial Intelligence','Machine Learning','Chatbots', 'Software', 'Data Science','Computers']
+feeds = ['https://techcrunch.com/tag/machine-learning/feed/','https://techcrunch.com/tag/artificial-intelligence/feed/','https://techcrunch.com/tag/python/feed/', 'https://techcrunch.com/tag/chatbots/feed/', 'https://techcrunch.com/tag/php/feed/', 'https://techcrunch.com/tag/cloud/feed/']
